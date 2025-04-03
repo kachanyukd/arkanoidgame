6d6549e47ed6876031e9bb511e0e194de7c788b9
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-04-03 19:04:08 +0300

    added paddle for platform control

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ pygame.display.set_caption("Arkanoid")
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+
+class Paddle:
+    def __init__(self):
+        self.rect = pygame.Rect(WIDTH // 2 - 50, HEIGHT - 20, 100, 10)
+    
+    def move(self, keys):
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.move_ip(-6, 0)
+        if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
+            self.rect.move_ip(6, 0)
+    
+    def draw(self):
+        pygame.draw.rect(screen, RED, self.rect)
